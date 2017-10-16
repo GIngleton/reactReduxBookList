bda99165819e14144eea599a566a6c432a0862d4
gingleton <georgeingleton@gmail.com>
2017-10-16 11:39:39 -0400

    Book list using react and redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title:'Slaughterhouse Five', pages: 101},
+    {title:'Invisible Man', pages: 182},
+    {title:'Clockwork Orange', pages: 157},
+    {title:'The Windup Bird Chronicles', pages: 202},
+    {title:'Crime and Punishment', pages: 327}
+  ]
+}
